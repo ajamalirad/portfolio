576801143d3b12e35c8569e04e0f9465e380f5d9
ajamalirad <aplasticlanguage@gmail.com>
2021-04-26 19:02:12 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const portfolioApp = {}
+
+// select the project list item from the html
+// on click on the project list item, a div appears with information on that project with
+    // --> image
+    // --> title, tools used
+    // --> link to github and live deploy
+
+// on scroll, a list of tool I can use appears
+
+
+
+
+portfolioApp.init = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+portfolioApp.init();

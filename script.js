5d2bef99bf171a9833c44bdc85033d3a95cc430a
ajamalirad <aplasticlanguage@gmail.com>
2021-04-27 17:04:09 -0400

    continued with styles

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-const portfolioApp = {}
+// const portfolioApp = {}
 
-// select the project list item from the html
-// on click on the project list item, a div appears with information on that project with
-    // --> image
-    // --> title, tools used
-    // --> link to github and live deploy
+// // select the project list item from the html
+// // on click on the project list item, a div appears with information on that project with
+//     // --> image
+//     // --> title, tools used
+//     // --> link to github and live deploy
 
-// on scroll, a list of tool I can use appears
+// // on scroll, a list of tools I can use appears
+
+// portfolioApp.init = () => {
+
+// }
+
+// // declare the variables
+// portfolioApp.projectThumbOne = document.querySelector('.projectOne');
+// portfolioApp.projectThumbTwo = document.querySelector('.projectTwo');
+// portfolioApp.arrow = document.querySelector('i');
+// portfolioApp.projectDiv = document.querySelector('.individualProject');
+
+// // portfolioApp.arrow.addEventListener('click', () => {
+
+// // })
+// console.log(portfolioApp.projectThumbOne);
+// console.log(portfolioApp.projectThumbTwo);
+
+
+// portfolioApp.projectThumbOne.addEventListener('click', () => {
+
+// })
 
 
 
 
-portfolioApp.init = () => {
-
-}
 
 
-
-
-
-
-
-
-
-
-portfolioApp.init();
+// portfolioApp.init();
